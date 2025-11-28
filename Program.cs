91fc475b1c5043270513274ad2b7440ca8fae7e1
giahuy <boomhuyxt@gmail.com>
2025-11-28 18:58:31 +0700

    cap nhat

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QL_KHO_HANG.form;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace QL_KHO_HANG
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new TRANGCHU());
+            Application.Run(new DangKy_dangnhap());
         }
     }
 }
---
+++ form/DangKy_dangnhap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QL_KHO_HANG.form
+{
+    public partial class DangKy_dangnhap : Form
+    {
+        public DangKy_dangnhap()
+        {
+            InitializeComponent();
+        }
+    }
+}
